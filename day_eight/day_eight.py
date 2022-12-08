c41d1f39559e7dfd30970c57264b1c4fcf0a98a4
Tommy <unconfigured@null.spigotmc.org>
2022-12-08 17:22:49 +0000

    part A done and clean

--- day_eight/day_eight.py
+++ day_eight/day_eight.py
@@ -1,12 +1,7 @@
 vis = 0
 with open("input.txt") as iFile:
     grid = [[int(x) for x in line if x != "\n"] for line in iFile.readlines()]
-    """grid = [[1,1,1,1,1],
-            [1,2,2,2,1],
-            [1,2,3,2,1],
-            [1,2,2,2,1],
-            [1,1,1,1,1],
-    ]"""
+
     height = len(grid)
     width = len(grid[0])
     
@@ -27,8 +22,6 @@ with open("input.txt") as iFile:
                 toAdd = 1
                 highestCols[x] = tree
 
-            #rowSoFar = [x for x in rowSoFar if x > tree]
-            #rowSoFar.append(tree)
 
             while rowSoFar and rowSoFar[-1][0] <= tree:
                 rowSoFar.pop()
@@ -36,11 +29,8 @@ with open("input.txt") as iFile:
             while colsSoFar[x] and colsSoFar[x][-1][0] <= tree:
                 colsSoFar[x].pop()
             
-            #tree height, score, position
             rowSoFar.append((tree, 1 - toAdd, x))
             colsSoFar[x].append((tree, 1 - toAdd, y))
-            #if toAdd:
-            #    print(f"Counted {tree, x, y}")
             vis += toAdd
 
         for treeC in rowSoFar:
@@ -48,14 +38,12 @@ with open("input.txt") as iFile:
             if treeC[1]:
                 x = treeC[2]
                 counted[x].add(y)
-                #print(f"Counted {treeC[0], x, y}")
 
     for x, col in enumerate(colsSoFar):
         for treeC in col:
             if treeC[1]:
                 y = treeC[2]
                 if y not in counted[x]:
-                    #print(f"Counted {treeC[0], x, y}")
                     vis += 1
 
 
